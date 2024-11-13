3f1be5a753cbe1637b6818377f0f153f1277dfda
mndev2019 <muskanvision2019@gmail.com>
2024-11-13 12:15:48 +0530

    tableheadcolorchange

--- src/Api/Base_url.js
+++ src/Api/Base_url.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://192.168.1.6:4321/"
-// export const BASE_URL = "https://iamparevartan.org:4321/"
+// export const BASE_URL = "https://192.168.1.6:4321/"
+export const BASE_URL = "https://iamparevartan.org:4321/"
